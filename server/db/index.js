6415ea87869b7050e53ec61da76e2824c3b33cd7
Joanna Cieszyńska <joannacieszynska33@gmail.com>
2021-01-20 18:08:41 +0100

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect("mongodb+srv://jcieszynska:zaq12WSX@cluster0.mvpto.mongodb.net/employees?retryWrites=true&w=majority",
+    .connect("mongodb+srv://login:password@cluster0.mvpto.mongodb.net/employees?retryWrites=true&w=majority",
     {useNewUrlParser: true, useUnifiedTopology: true})
     .catch(e => {
         console.error("Connection error", e.message);
